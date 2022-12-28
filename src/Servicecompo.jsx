cb35187c6aef67f686b566367c2e18294f67cbc5
gaurabh kumar jha <gaurabhkumarjha27102001@gmail.com>
2022-12-28 14:42:18 +0530

    check

--- src/Servicecompo.jsx
+++ src/Servicecompo.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './servicecompo.css'
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Servicecompo = (props) => {
+    const { isAuthenticated } = useAuth0();
 
+    function getpopup() {
 
+       if (isAuthenticated)
+            window.location.href = "/Details";
+       else alert("login");
+    }
     return (
 
         <>
-                        <div class="card" style={{ width: '18rem' }}>
-                            <img src={props.cimg} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">{props.cname}</h5>
-                                <p class="card-text">{props.cdescription}</p>
-                                <a href= "/Details" class="btn btn-warning newbtn">Get More Details</a>
-                            </div>
-                        </div>
+            <div class="card" style={{ width: '18rem' }}>
+                <img src={props.cimg} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">{props.cname}</h5>
+                    <p class="card-text">{props.cdescription}</p>
+                    <button type="button" onClick={getpopup} class="btn btn-warning newbtn">Get More Details</button>
+                </div>
+            </div>
         </>
     );
 };
